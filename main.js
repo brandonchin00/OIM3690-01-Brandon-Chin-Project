af96553a779811d95b1b9993622c3c0ca09251e4
brandonchin00 <brandonzchin@gmail.com>
2022-11-01 01:37:14 -0400

    1

--- main.js
+++ main.js
@@ -20,3 +20,22 @@ function revealItem(){
 }
 
 window.alert("This is Website is a Work-Progress, Subject to Change");
+
+let stars = document.getElementById('stars');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.marginTop = value * 0.5 + 'px';
+});
+
+document.getElementById('title').addEventListener('mouseover', function titleOver (){
+    const job = document.getElementById('title');
+    job.innerHTML = "Babson College Student";
+    job.style.color = "green";
+});
+
+document.getElementById('title').addEventListener('mouseout', function titleOver (){
+    const job = document.getElementById('title');
+    job.innerHTML = "Product Manager";
+    job.style.color = "rgb(197, 197, 197)";
+});
