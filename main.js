35b5cc636c26c3b3cd55f36c29686e4205ad5ca7
brandonchin00 <brandonzchin@gmail.com>
2022-12-07 00:00:43 -0500

    mainjs

--- main.js
+++ main.js
@@ -28,28 +28,46 @@ let cloud = document.getElementById('cloud');
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     stars.style.marginTop = value * 0.5 + 'px';
+    cloud.style.marginTop = value * -0.10 + 'px';
 
 });
 
+window.addEventListener("load", () => {
+    word();
+});
 
 //title change
 document.getElementById('title').addEventListener('mouseover', function titleOver (){
     const job = document.getElementById('title');
-    job.innerHTML = "Babson College Student";
+    job.innerHTML = "Babson College";
     job.style.color = "green";
+    job.style.font = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
 });
 
 document.getElementById('title').addEventListener('mouseout', function titleOver (){
     const job = document.getElementById('title');
-    job.innerHTML = "Product Manager";
-    job.style.color = "rgb(197, 197, 197)";
+    job.innerHTML = "Business School";
+    job.style.color = "white";
+    job.style.font = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
+});
+
+window.addEventListener("scroll", () => {
+    running();
 });
 
 
-window.addEventListener('scroll', () => {
-    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+function running() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = (scroll / height) * 100;
+    let scrolled = (winScroll / height) * 100;
     document.getElementById("move").style.width = scrolled + "%";
-});
+  }
 
+function word(){
+    let typed = new Typed(".auto-input", {
+        strings: ["Brandon Chin", "a Product Manager", "a Student Developer", "a Part-Time Swim Instructor"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true,
+    });
+}
